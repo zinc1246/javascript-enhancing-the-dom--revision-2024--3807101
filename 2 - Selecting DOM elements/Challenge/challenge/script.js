7d8d0d88f2b505f476089854469037800d506ad5
Lee Yan Lin <s10268861c@gmail.com>
2025-11-26 15:33:35 +0800

    Complete challenge

--- 2 - Selecting DOM elements/Challenge/challenge/script.js
+++ 2 - Selecting DOM elements/Challenge/challenge/script.js
@@ -1,11 +1,24 @@
 // Challenge 1: Select an element by ID and print it to the console
 // TODO: Select the element with id 'actionButton' and print it
+let actBtn = document.getElementById('actionButton');
+console.log(actBtn);
 
 // Challenge 2: Select all elements with a certain class and print all of them to the console
 // TODO: Select all elements with class 'info' and print each one
+let infoElements = document.getElementByClassName('info');
+for (let i = 0; i < infoElements.length; i++)
+{
+  console.log(infoElements[i]);
+}
 
 // Challenge 3: Select elements by tag and print all of them to the console
 // TODO: Select all <p> elements and print each one
-
+let paragraphs = document.getElementByTagName('p');
+for (let i = 0; i < paragraphs.length; i++)
+{
+  console.log(paragraphs[i]);
+}
 // Challenge 4: Select elements with a combination of tag name and class name, and print them
 // TODO: Select all elements that are <span> with class 'highlight' and print each one
+let highlight = document.querySelectorAll('span.highlight');
+highlight.forEach(span => console.log(span));

5cabc39236e6582d747df015582a66446c8f186a
Lee Yan Lin <s10268861c@gmail.com>
2025-11-26 16:02:18 +0800

    Update script.js

--- 4 - Modifying DOM elements/Challenge/Challenge/script.js
+++ 4 - Modifying DOM elements/Challenge/Challenge/script.js
@@ -1,7 +1,14 @@
 function changeStyle() {
     // Challenge Instructions:
     // 1. When the button is clicked, change the background color of 'dynamicText' to 'lightblue'.
+    let isBold = false;
+    let dynamicText = document.getElementById('dynamicText');
+    dynamicText.style.backgroundColor = 'lightblue';
     // 2. Change the font size of 'dynamicText' to '18px'.
+    dynamicText.style.fontSize = '18px';
     // 3. Toggle the font weight of 'dynamicText' between 'normal' and 'bold' on each button click.
+    dynamicText.style.fontweight = isBold ? 'normal' : 'bold';
+    isBold = !isBold;
     // 4. Change the text color of 'dynamicText' to 'green'.
+    dynamicText.style.color = 'green';
 }

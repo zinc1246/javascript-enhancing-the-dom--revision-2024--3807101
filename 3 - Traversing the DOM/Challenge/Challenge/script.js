17f1db60203db307901f0d4852646230e3d288f3
Lee Yan Lin <s10268861c@gmail.com>
2025-11-30 17:30:42 +0800

    Update script.js

--- 3 - Traversing the DOM/Challenge/Challenge/script.js
+++ 3 - Traversing the DOM/Challenge/Challenge/script.js
@@ -1,6 +1,10 @@
 // Your tasks:
 // 1. Find the parent element of a cat and log it to the console.
-
+let cat = document.querySelector('.cat');
+console.log(cat.parentElement);
 // 2. List all the children of the cat’s parent and log them.
-
+let catParent = cat.parentElement;
+let parentChildren = Array.from(catParent.children);
+parentChildren.forEach(c => console.log(c));
 // 3. Identify the cat’s parent's next sibling in the tree and log it.
+console.log(catParent.nextElementSibling);

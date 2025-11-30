72cb10275ab8d9d8a2d17c56f62e2c87fbba5e46
Lee Yan Lin <s10268861c@gmail.com>
2025-11-30 17:16:22 +0800

    Correct DOM element selection and iteration logic
    Fixed element selection methods and loop syntax errors.

--- 2 - Selecting DOM elements/Challenge/challenge/script.js
+++ 2 - Selecting DOM elements/Challenge/challenge/script.js
@@ -1,18 +1,22 @@
 // Challenge 1: Select an element by ID and print it to the console
 // TODO: Select the element with id 'actionButton' and print it
-actbtn = getElementById(actionButton);
+actbtn = getElementById('actionButton');
 console.log(actbtn);
 
 // Challenge 2: Select all elements with a certain class and print all of them to the console
 // TODO: Select all elements with class 'info' and print each one
-infoList = getElementbyClass(info);
-for (i = 0; i < infoList.index; i++)
+infoList = getElementbyClass('info');
+for (let i = 0; i < infoList.length; i++)
 {
   console.log(infoList[i]
-              }
-
+}
 // Challenge 3: Select elements by tag and print all of them to the console
 // TODO: Select all <p> elements and print each one
-
+tagElement = getElementsByTag('p')
+for (let i = 0; i < tagElement.length; i++)
+{
+  console.log(tagElement[i])
+}
 // Challenge 4: Select elements with a combination of tag name and class name, and print them
 // TODO: Select all elements that are <span> with class 'highlight' and print each one
+
